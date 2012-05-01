eb499ce0342a03a26324a6f89ea4e76dc09c28d9
Ammar Lakis <ammar.lakis@gmail.com>
2012-05-01 08:41:39 +0300

    Add Create Pdiff and a namespace fix in Math_Expression

---
+++ Numerical Methods/Math_Expression.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Numerical_Methods
+{
+	public class Math_Expression{
+		
+	    
+		Stack Mexper = new Stack();
+		Stack Sub_Mexper = new Stack();
+		public Math_Expression ()
+		{
+		
+		}
+	}
+}
+
