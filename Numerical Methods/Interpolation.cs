d40393bc4b42caf7e5953dd9bc87f166b8f41157
Ammar Lakis <ammar.lakis@gmail.com>
2012-05-09 06:05:25 +0000

    Lagrange added

--- Numerical Methods/Interpolation.cs
+++ Numerical Methods/Interpolation.cs
@@ -15,7 +15,7 @@ namespace Numerical_Methods
 		/// Points table.
 		/// </param>
 		public static double[,] CreatPdiffTable(double[,] pTable)
-        {
+        	{
 			double[,] PdiffTable = new double[pTable.GetLength(1) - 1, pTable.GetLength(1) - 1];
 			if (Integration.CheckPointTable(pTable))
 			{
@@ -29,7 +29,7 @@ namespace Numerical_Methods
 				}
 			}
 			return PdiffTable;
-        }
+	        }
 		/// <summary>
 		/// Calculates a cell's value in progressive differations table.
 		/// </summary>
@@ -45,41 +45,89 @@ namespace Numerical_Methods
 		/// <param name='index'>
 		/// Index.
 		/// </param>
-        public static double CalcPdiff(double[,] pTable, int rank, int index)
-        {
-            double ans = 0;
-            for (int k = 0; k <= rank; k++)
-            {
-                double s = Interpolation.Factorial(rank) / (Interpolation.Factorial(rank - k) * Interpolation.Factorial(k));
-                ans += s * Math.Pow(-1, k) * pTable[1, index + rank - k];
-            }
-            return ans;
-        }
-        public static double Factorial(double num)
-        {
-            if (num == 1 || num == 0)
-            {
-                return 1;
-            }
-            else
-            {
-                return Factorial(num - 1) * num;
-            }
-        }
-       /*public static double Lagrange(double [,] pointsTable)
-        {
-            double sum = 0, l;
-            for (int i = 0; i < pointsTable.GetLength(1); i++)
-            {
-                l = 1;
-                for (int j = 0; j < pointsTable.GetLength(1); j++)
-                {
-                    l *= (x - pointsTable[0, j])/(pointsTable[0, 0] - pointsTable[0, i]);
-                }
-                sum += pointsTable[1, i]*l;
-            }
-            return sum;
-        }*/
+		public static double CalcPdiff(double[,] pTable, int rank, int index)
+		{
+	            double ans = 0;
+	            for (int k = 0; k <= rank; k++)
+	            {
+	                double s = Interpolation.Factorial(rank) / (Interpolation.Factorial(rank - k) * Interpolation.Factorial(k));
+	                ans += s * Math.Pow(-1, k) * pTable[1, index + rank - k];
+	            }
+	            return ans;
+	        }
+	        
+		/// <summary>
+		/// Calculationg interpolation by Lagrange method.
+		/// </summary>
+		/// <param name='pTable'>
+		/// Function's values table.
+		/// </param>
+		public static double Lagrange (double[,] pTable)
+		{
+			for (int i = 0; i < pTable.GetLength(1);i++)
+			{
+				return pTable[1,i]*Lagrange1Derivative(pTable.GetLength(1),i,Numerical_Methods);
+			}
+		}
+		
+		/// <summary>
+		/// Lagrange first derivative.
+		/// </summary>
+		/// <returns>
+		/// The derivative.
+		/// </returns>
+		/// <param name='M'>
+		/// Values number.
+		/// </param>
+		/// <param name='i'>
+		/// Lagrange's polynomynal degree.
+		/// </param>
+		/// <param name='num'>
+		/// Entered value.
+		/// </param>
+		private static double Lagrange1Derivative(int M, int i, double num)
+		{
+			double [] s = new double[M]();
+			for (int c = 0; c < M; c++)
+			{
+				s[c] = -1+2	*(i-1)/(M-1);
+			}
+			for (int k = 0; k < M; k++)
+			{
+				double sum = 0d;
+				double mult = 1d;
+				if (k != i)
+				{
+					sum += 1/(s[i]-s[k]);
+				}
+				for (int j = 0; j < M; j++)
+				{
+					if ((j != i) && (j != k))
+					{
+						mult *= (num-s[j])/(s[i]-s[j]);
+					}
+				}
+				return sum*mult;
+			}
+		}
+	
+		/// <summary>
+		/// Calculates the factorial for the specified num.
+		/// </summary>
+		/// <param name='num'>
+		/// Number.
+		/// </param>
+		public static double Factorial(double num)
+	        {
+	            if (num == 1 || num == 0)
+	            {
+	                return 1;
+	            }
+	            else
+	            {
+	                return Factorial(num - 1) * num;
+	            }
+	        }
     }
 }
 
