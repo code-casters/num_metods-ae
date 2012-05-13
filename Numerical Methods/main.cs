147a7492b779d961d70f26f9ac3c23094e83037b
Ammar Lakis <ammar.lakis@gmail.com>
2012-05-13 16:32:01 +0300

    checktable problem solved

--- Numerical Methods/main.cs
+++ Numerical Methods/main.cs
@@ -13,26 +13,6 @@ namespace Numerical_Methods
         public MainForm()
         {
             InitializeComponent();
-            //double[,] pt = new double[2, 7];
-            //pt[0, 0] = 30; pt[1, 0] = 0.5;
-            //pt[0, 1] = 35; pt[1, 1] = 0.5736;
-            //pt[0, 2] = 40; pt[1, 2] = 0.6428;
-            //pt[0, 3] = 45; pt[1, 3] = 0.7071;
-            //pt[0, 4] = 50; pt[1, 4] = 0.7660;
-            //pt[0, 5] = 55; pt[1, 5] = 0.8192;
-            //pt[0, 6] = 60; pt[1, 6] = 0.8660;
-            //MessageBox.Show(Functions.Postfix("a+c+b"));
-            //MathBinaryTree te = new MathBinaryTree("5+x");
-            //MathBinaryTree dte = te.Drive(te.Root);
-            //double[,] pt = new double[2, 7];
-            //pt[0, 0] = 30; pt[1, 0] = 0.5;
-            //pt[0, 1] = 35; pt[1, 1] = 0.5736;
-            //pt[0, 2] = 40; pt[1, 2] = 0.6428;
-            //pt[0, 3] = 45; pt[1, 3] = 0.7071;
-            //pt[0, 4] = 50; pt[1, 4] = 0.7660;
-            //pt[0, 5] = 55; pt[1, 5] = 0.8192;
-            //pt[0, 6] = 60; pt[1, 6] = 0.8660;
-            //MessageBox.Show(@"Rectangles method : " + Integration.Rects(pt).ToString() + @" , Trapezoid method : " + Integration.Trapezoid(pt).ToString() + @" , Simpson method : "+ Integration.Simpson(pt).ToString());
         }
 
         private void TxtbxNumEnter(object sender, EventArgs e)
@@ -81,8 +61,8 @@ namespace Numerical_Methods
             _pTable = new double[datgridTable.Rows.GetLastRow(DataGridViewElementStates.Visible), 2];
             for (int i = 0; i < datgridTable.Rows.GetLastRow(DataGridViewElementStates.Visible); i++)
             {
-                if ((!double.TryParse(datgridTable[0, i].ToString(), out _pTable[i, 0])) ||
-                    (!double.TryParse(datgridTable[1, i].ToString(), out _pTable[i, 1])))
+                if ((!double.TryParse(datgridTable[0, i].Value.ToString(), out _pTable[i, 0])) ||
+                    (!double.TryParse(datgridTable[1, i].Value.ToString(), out _pTable[i, 1])))
                 {
                     return false;
                 }
